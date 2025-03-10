c0f728771748a584fea16f4f320d80884c12e607
zishan344 <zishanahmed344@gmail.com>
2025-03-10 12:16:18 +0600

    Add API URL routing and home view; update settings for PostgreSQL

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import JsonResponse
+def home (request):
+  " Create a simple home rendering view return welcome to skillNestify."
+  return JsonResponse({"message":"Welcome to the skillNestify"})
