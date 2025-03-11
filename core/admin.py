30b2f10b790900510a465dc77b668c751b433654
zishan344 <zishanahmed344@gmail.com>
2025-03-11 09:03:53 +0600

    Update CustomUserAdmin to include first and last name fields; add requirements.txt for project dependencies

--- core/admin.py
+++ core/admin.py
@@ -5,20 +5,20 @@ from core.models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
   model = CustomUser
-  list_display = ['email','username','user_type', 'is_staff']
-  list_filter = ['is_staff','is_active']
+  list_display = ['email', 'username', 'first_name', 'last_name', 'user_type', 'is_staff']
+  list_filter = ['is_staff', 'is_active']
   fieldsets = (
-    (None, {'fields': ('email', 'username', 'first_name', 'last_name', 'password')}),
-    ('Personal_Info', {'fields': ('user_type','address','phone_number')}),
-    ('Permissions', {'fields': ('is_staff', 'is_active','is_superuser','groups','user_permissions')}),
-    ('Important_dates', {'fields': ('last_login',)}),  # Removed 'date_joined' from here
+    (None, {'fields': ('email', 'username', 'password')}),
+    ('Personal_Info', {'fields': ('first_name', 'last_name', 'user_type', 'address', 'phone_number')}),
+    ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    ('Important_dates', {'fields': ('last_login',)}),
   )
   add_fieldsets = (
     (None, {
       'classes': ('wide',),
-      'fields': ('email','username','first_name','last_name','password1','password2','user_type','address','phone_number','is_staff','is_active')
+      'fields': ('email', 'username', 'password1', 'password2', 'first_name', 'last_name', 'user_type', 'address', 'phone_number', 'is_staff', 'is_active')
     }),
   )
-  search_fields = ['email','username']
+  search_fields = ['email', 'username', 'first_name', 'last_name']
   ordering = ['email']
 admin.site.register(CustomUser, CustomUserAdmin)
